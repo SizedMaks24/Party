105eb83c6137b788d0f4eb629f21981b80b10f4d
SizedMaks24 <shvint74@gmail.com>
2018-05-11 15:01:59 +0300

    hjdfjhdf
    dfdfdfdfdfdfd

--- admin.php
+++ admin.php
@@ -13,10 +13,10 @@ $app = new \atk4\ui\App('Админка');
 $app->initLayout('Centered');
 
 $db = new
-\atk4\data\Persistence_SQL('mysql:dbname=fdb;host=localhost','root','') ;
+\atk4\data\Persistence_SQL('mysql:dbname=heroku_35a63eb77fee245;host=eu-mm-auto-fra-02-c.cleardb.net','bfa11752c77b7d','77c43c35') ;
 
 Class Friends extends \atk4\data\Model {
-  public $table ='friends' ;
+  public $table ='Maks' ;
   function init () {
 parent::init() ;
   $this->addField('name') ;
