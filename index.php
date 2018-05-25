eb998419aa870eac6442017ccb043abe56f512ef
SizedMaks24 <shvint74@gmail.com>
2018-05-25 14:09:48 +0300

    fddfdfd
    fdfdfdfdf

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $db = new
 \atk4\data\Persistence_SQL('mysql:dbname=heroku_35a63eb77fee245;host=eu-mm-auto-fra-02-c.cleardb.net','bfa11752c77b7d','77c43c35') ;
 
 Class Friends extends \atk4\data\Model {
-  public $table ='maks' ;
+  public $table ='maks2' ;
   function init () {
 parent::init() ;
   $this->addField('name') ;
